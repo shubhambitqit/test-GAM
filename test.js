c2994f3d6e8600fefc83e82b19cffdabc10f5963
shubhambitqit <shubham@bitqit.com>
2024-02-20 11:26:25 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,27 @@
+var _cio = _cio || [];
+(function() {
+    var a, b, c;
+    a = function(f) {
+        return function() {
+            _cio.push([f].concat(Array.prototype.slice.call(arguments, 0)));
+        };
+    };
+    b = ["load", "identify", "sidentify", "track", "page", "on", "off"];
+    for (c = 0; c < b.length; c++) {
+        _cio[b[c]] = a(b[c]);
+    }
+    var t = document.createElement('script'),
+        s = document.getElementsByTagName('script')[0];
+    t.async = true;
+    t.id = 'cio-tracker';
+    t.setAttribute('data-site-id', 'YOUR_SITE_ID');
+    t.setAttribute('data-use-array-params', 'true');
+
+    // Enables in-app messaging
+    t.setAttribute('data-use-in-app', 'true');
+
+    t.src = 'https://assets.customer.io/assets/track.js';
+    // If your account is in the EU, use:
+    // t.src = 'https://assets.customer.io/assets/track-eu.js'
+    s.parentNode.insertBefore(t, s);
+})();
